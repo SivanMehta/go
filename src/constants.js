9b7c84dfefbaf0f6843fa7c8063f2ad1835bbf0b
Sivan Mehta <sivanmehta@microsoft.com>
2022-08-15 18:42:16 -0700

    liberties

--- src/constants.js
+++ src/constants.js
@@ -1,19 +1,5 @@
-export const SIZE = 5;
+export const SIZE = 8;
 export const CELL_SIZE = 50;
-
-
-// check if a piece can be placed at a given location
-export function canBePlaced(state, row, col) {
-  if(state.over) return false;
-  if(state.pieces[row][col]) return false;
-
-  const copy = JSON.parse(JSON.stringify(state.pieces));
-  copy[row][col] = state.player;
-  const hash = hashBoard({ pieces: copy });
-  if(hash in state.hashes) return false;
-
-  return true;
-}
 
 export function toggleTurn(color) {
   if(color === 'black') {
@@ -44,4 +30,55 @@ export function addToBoard(state, row, col) {
   const hash = hashBoard(state);
   state.hashes[hash] = true;
   return [state.hashes, state.pieces];
+}
+
+const directions = [
+  [0, 1], // right
+  [0, -1], // left
+  [1, 0], // down
+  [-1, 0], // up
+];
+
+// a spot has liberties if it is adjacent to an empty square or a friendly piece
+function hasLiberties(state, row, col) {
+  for(let i = 0; i < directions.length; i ++) {
+    // looking at ourselves
+    const [ dx, dy ] = directions[i];
+    if(dx === 0 && dy === 0) continue;
+    const r = row + dy;
+    const c = col + dx;
+    // if the spot is out of bounds, we don't care
+    if(r < 0 || r >= SIZE || c < 0 || c >= SIZE) {
+      continue;
+    }
+
+    // if the spot is empty, we have a liberty
+    if(!state.pieces[r][c]) {
+      return true;
+    }
+    
+    // if the spot is friendly, we have a liberty
+    if(state.pieces[r][c] === state.player) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// check if a piece can be placed at a given location
+export function canBePlaced(state, row, col) {
+  if(state.over) return false;
+  if(state.pieces[row][col]) return false;
+
+  // check for liberties of given pieces
+  const liberated = hasLiberties(state, row, col);
+  if(!liberated) return false;
+
+  const copy = JSON.parse(JSON.stringify(state.pieces));
+  copy[row][col] = state.player;
+  const hash = hashBoard({ pieces: copy });
+  if(hash in state.hashes) return false;
+  
+
+  return true;
 }
